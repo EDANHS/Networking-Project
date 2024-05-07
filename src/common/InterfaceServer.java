46b09d6f73d872ef24aab0deff63ced9c1db5cdd
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-05-07 19:48:57 -0400

    add new functions, add_users and add_data

--- src/common/InterfaceServer.java
+++ src/common/InterfaceServer.java
@@ -2,12 +2,14 @@ package common;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface InterfaceServer extends Remote {
 	public Boolean log_in(String email, String password) throws RemoteException;
-	public Boolean make_transaction(String origin_id, String dest_id, int total, String currency_type) throws RemoteException;
+	public Boolean make_transaction(String origin_id, String dest_id, int total, String currency) throws RemoteException;
 	public void show_transactions(String id) throws RemoteException;
 	public ArrayList<User> getUsers() throws RemoteException;
-	public Boolean add_user(String name, String rut, String email, String password);
+	public Boolean add_user(String name, String rut, String email, String password) throws RemoteException;
+	public Boolean add_data() throws RemoteException, SQLException;
 }
