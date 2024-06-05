3f12a7a013ba62b645238ad1ab36377940535714
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-05 04:02:31 -0400

    add function to change server in case of any problem

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,6 @@
 package client;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.rmi.ConnectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
